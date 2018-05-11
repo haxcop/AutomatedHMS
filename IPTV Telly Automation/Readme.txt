5126fc7de5d58acc5566ad5c8beede9377b3c64a
Carlos Lozada <clozada@asavie.com>
2018-05-11 16:22:42 +0100

    initial state

---
+++ IPTV Telly Automation/Readme.txt
@@ -0,0 +1,3 @@
+Download the folder IPTV Telly Automation on your desired location, Preferible on "\my documents" folder
+Run the file automated.bat and follow the instructions
+
---
+++ IPTV Telly Automation/automated.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
+You Welcome https://youtu.be/79DijItQXMM
+PAUSE
